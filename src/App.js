c8344d7c65c608b7ba4a9077be8a9aa7d690b68d
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-08-20 20:58:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState(' ');
+  const [recipes, setRecipes] = useState([]);
+
+  const APP_ID = '498eb447';
+  const APP_KEY = 'd2ea386e46a40fa64a051cd43b50068f';
+  const API_URL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const searchRecipes = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setRecipes(response.data.hits);
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:"tomato",width:"80%",height:"80%",marginLeft:"10%"}}>
+      <h1>Recipe App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Search for recipes..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button onClick={searchRecipes}>Search</button><br></br>
+        <h>search any food item</h>
+      </div>
+      <div style={{display:"flex",flexWrap:"wrap", justifyContent:"space-between"}}>
+        {recipes.map((recipe, index) => (
+          <div key={index}>
+            <h2>{recipe.recipe.label}</h2>
+            <img src={recipe.recipe.image} alt={recipe.recipe.label} />
+            <p>Calories: {recipe.recipe.calories.toFixed(2)}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
